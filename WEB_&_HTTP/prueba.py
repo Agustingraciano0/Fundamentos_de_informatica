1e22e6cca7400482fe3199800d7bb7b08d1aa35a
Agustingraciano0 <101571968+Agustingraciano0@users.noreply.github.com>
2022-06-23 11:31:59 -0300

    agregue

---
+++ WEB_&_HTTP/prueba.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import scipy.stats as ss
+import pandas as pd
+import ipywidgets as widgets
+import voila
+import jupyter
+import sklearn
+
+personas=pd.read_csv("personas_2011.csv")
